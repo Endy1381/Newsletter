def6a8c1639f43caf368b16f97cea3e2166470cc
Endy1381 <119288346+Endy1381@users.noreply.github.com>
2024-05-04 12:57:00 +0300

    update

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function handleClicka(){
+    let fform = document.querySelector('.fform')
+    let success = document.querySelector('.success')
+    fform.style.display === 'none'
+    success.style.display === 'flex'
+
+}
