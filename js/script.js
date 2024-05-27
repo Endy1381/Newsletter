3aaae5fb62dc7390d8e89d59323e50d91933416f
Endy1381 <119288346+Endy1381@users.noreply.github.com>
2024-05-27 19:52:42 +0300

    update style.css
    just a hassle with the responsive part got it done except the image

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 function handleClicka(){
     let fform = document.querySelector('.fform')
     let success = document.querySelector('.success')
-    fform.style.display === 'none'
-    success.style.display === 'flex'
+    fform.style.display = 'none'
+    success.style.display = 'flex'
+
+}
+
+function handleClickb(){
+    let fform = document.querySelector('.fform')
+    let success = document.querySelector('.success')
+    fform.style.display = 'flex'
+    success.style.display = 'none'
 
 }
