a73810556cbe16a115cc6033a81821a82dcee6d6
Endy1381 <119288346+Endy1381@users.noreply.github.com>
2024-05-28 18:13:44 +0300

    Update
    i updated the image on the mobile screen so that it can appear on the top, by making another header on the html
    just see the code for yourself

--- js/script.js
+++ js/script.js
@@ -1,15 +1,47 @@
+let imgElement = document.querySelector('.img')
+let fform = document.querySelector('.fform')
+let success = document.querySelector('.success')
+let hh = document.querySelector('.header-mobile')
+
+
+
+// Get a reference to the image element
+
+
+// Function to update the image source
+function updateImageSource() {
+    const windowWidth = window.innerWidth;
+    if (windowWidth < 600) {
+        // Set the new image source for mobile
+        imgElement.src = '../assetsimages/illustration-sign-up-mobile.svg';
+    } else {
+        // Set the original image source for desktop
+        imgElement.src = '../assets/images/illustration-sign-up-desktop.svg';
+    }
+}
+
+// Initial check when the page loads
+updateImageSource();
+
+// Add an event listener for window resize
+window.addEventListener('resize', updateImageSource);
+
+
+
+
+
 function handleClicka(){
-    let fform = document.querySelector('.fform')
-    let success = document.querySelector('.success')
+    
     fform.style.display = 'none'
     success.style.display = 'flex'
+    hh.style.display = 'none'
 
 }
 
 function handleClickb(){
-    let fform = document.querySelector('.fform')
-    let success = document.querySelector('.success')
     fform.style.display = 'flex'
     success.style.display = 'none'
+    hh.style.display = 'flex'
 
 }
+
