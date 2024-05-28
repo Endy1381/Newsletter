c5a16df31b36792d8dae9b7998d4c9094b9854fd
Endy1381 <119288346+Endy1381@users.noreply.github.com>
2024-05-28 18:36:24 +0300

    Update
    removed unnecessary codes from the Js, and i edited the markdown

--- js/script.js
+++ js/script.js
@@ -8,23 +8,7 @@ let hh = document.querySelector('.header-mobile')
 // Get a reference to the image element
 
 
-// Function to update the image source
-function updateImageSource() {
-    const windowWidth = window.innerWidth;
-    if (windowWidth < 600) {
-        // Set the new image source for mobile
-        imgElement.src = '../assetsimages/illustration-sign-up-mobile.svg';
-    } else {
-        // Set the original image source for desktop
-        imgElement.src = '../assets/images/illustration-sign-up-desktop.svg';
-    }
-}
 
-// Initial check when the page loads
-updateImageSource();
-
-// Add an event listener for window resize
-window.addEventListener('resize', updateImageSource);
 
 
 
